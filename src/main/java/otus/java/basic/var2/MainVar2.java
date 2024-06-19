fa4db68f0a73ff81dbb74363fbe613451b2f19e6
Rexice23 <leouniverse@mail.ru>
2024-06-19 15:56:54 +0300

    Работа с многопоточностью.

--- src/main/java/otus/java/basic/var2/MainVar2.java
+++ src/main/java/otus/java/basic/var2/MainVar2.java
@@ -5,36 +5,41 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MainVar2 {
-        public static void main(String[] args) {
-            double[] array = new double[100_000_000];
+    public static void main(String[] args) {
+        double[] array = new double[100_000_000];
 
-            ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-            long start = System.nanoTime();
+        long start = System.nanoTime();
 
-            for (int i = 0; i < 4; i++) {
-                int startIdx = i * 25_000_000;
-                int endIdx;
-                if (i == 3) {
-                    endIdx = array.length;
-                } else {
-                    endIdx = (i + 1) * 25_000_000;
+        for (int i = 0; i < 4; i++) {
+            int startIdx = i * 25_000_000;
+            int endIdx;
+            if (i == 3) {
+                endIdx = array.length;
+            } else {
+                endIdx = (i + 1) * 25_000_000;
+            }
+            executor.submit(() -> {
+                for (int j = startIdx; j < endIdx; j++) {
+                    array[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j / 1.2);
                 }
-                executor.submit(() -> {
-                    for (int j = startIdx; j < endIdx; j++) {
-                        array[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j / 1.2);
-                    }
-                });
-            }
-
-            executor.shutdown();
-            while (!executor.isTerminated()) {
-            }
-
-            long end = System.nanoTime();
-
-            long time = end - start;
-            System.out.println("Время выполнения: " + TimeUnit.NANOSECONDS.toSeconds(time) + " секунд");
+            });
         }
-    }
 
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.HOURS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        long end = System.nanoTime();
+
+        long time = end - start;
+        System.out.println("Время выполнения: " + TimeUnit.NANOSECONDS.toSeconds(time) + " секунд");
+    }
+}
